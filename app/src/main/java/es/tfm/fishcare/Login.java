aa3a6f0f76e0d6f905e6ae2cf53913da5d3379a9
Alejandro <alejandro.gonzalez3@udc.es>
2021-02-04 20:28:28 +0100

    [LOGIN] Login Activity + Layout: New fish icon (_blue, older change to white by defaul) Rounded shape (rounded_buton) More small changes

---
+++ app/src/main/java/es/tfm/fishcare/Login.java
@@ -0,0 +1,66 @@
+package es.tfm.fishcare;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class Login extends AppCompatActivity {
+    EditText loginName, loginPassword;
+    Button login;
+    TextView textViewSignup;
+    ImageView loginFish;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        loginName = findViewById(R.id.loginName);
+        loginPassword = findViewById(R.id.loginPassword);
+        login = findViewById(R.id.loginButton);
+        textViewSignup = findViewById(R.id.textViewSignup);
+        loginFish = findViewById(R.id.loginFish);
+
+        loginName.bringToFront();
+        loginPassword.bringToFront();
+        loginFish.bringToFront();
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                checkFormData();
+            }
+        });
+
+        textViewSignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), Signup.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    boolean isEmpty(EditText field) {
+        String text = field.getText().toString();
+        return (TextUtils.isEmpty(text));
+    }
+
+    protected void checkFormData() {
+        if (isEmpty(loginName)) {
+            loginName.setError("Login name is required");
+        }
+
+        if (isEmpty(loginPassword)) {
+            loginPassword.setError("Password is required");
+        }
+
+    }
+}

73804139c3f59e3fe1404c8b3dcb117112f6601e
Alejandro <alejandro.gonzalez3@udc.es>
2021-07-09 19:06:15 +0200

    [ACTIONS] Integrate Actions: * New static methods on RestService (get actions urls) * Get All the actions from backend * Do Backend call to On/Off the actuators * Some fixes

--- app/src/main/java/es/tfm/fishcare/mainFragments/ActionsFragment.java
+++ app/src/main/java/es/tfm/fishcare/mainFragments/ActionsFragment.java
@@ -8,15 +8,32 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ListView;
+import android.widget.Toast;
 
+import com.github.mikephil.charting.data.Entry;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.io.IOException;
+import java.lang.reflect.Type;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 import es.tfm.fishcare.Action;
 import es.tfm.fishcare.ActionsListAdapter;
 import es.tfm.fishcare.R;
+import es.tfm.fishcare.RestService;
 import es.tfm.fishcare.SensorValue;
 import es.tfm.fishcare.SensorValueState;
 import es.tfm.fishcare.notifications.NotificationListAdapter;
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.HttpUrl;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -24,16 +41,11 @@ import es.tfm.fishcare.notifications.NotificationListAdapter;
  * create an instance of this fragment.
  */
 public class ActionsFragment extends Fragment {
-
+    OkHttpClient client = RestService.getClient();
     ListView list;
 
     public ActionsFragment() {
         // Required empty public constructor
-    }
-
-    private Action[] getActions() {
-        // Get allowed actions of the system
-        return new Action[]{new Action("Oxygenator", true), new Action("Water pump", false)};
     }
 
     public static ActionsFragment newInstance(String param1, String param2) {
@@ -57,7 +69,47 @@ public class ActionsFragment extends Fragment {
     public void onViewCreated(View view, Bundle savedInstanceState) {
         // Setup any handles to view objects here
         list = view.findViewById(R.id.actionsList);
-        ActionsListAdapter adapter = new ActionsListAdapter(getActivity(), getActions());
-        list.setAdapter(adapter);
+        getActuators();
+    }
+
+    private void getActuators() {
+        HttpUrl.Builder urlBuilder;
+        urlBuilder = RestService.getActuatorUrlBuilder();
+        urlBuilder.addPathSegment("all");
+        String url = urlBuilder.build().toString();
+
+        Request request = new Request.Builder().url(url).build();
+
+        final Gson gson = new Gson();
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                e.printStackTrace();
+            }
+
+            @Override
+            public void onResponse(Call call, final Response response) throws IOException {
+                System.out.println(response);
+                if (!response.isSuccessful()) {
+                    throw new IOException("Unexpected code " + response);
+                }
+
+                Type listOfMyClassObject = new TypeToken<ArrayList<Action>>() {}.getType();
+                List<Action> actions = gson.fromJson(response.body().charStream(), listOfMyClassObject);
+                Action[] actionsArray = new Action[actions.size()];
+                int i = 0;
+                for (Action action : actions) {
+                    actionsArray[i] = action;
+                    i+=1;
+                }
+
+                // UI update must be done on Ui Thread
+                getActivity().runOnUiThread(() -> {
+                    ActionsListAdapter adapter = new ActionsListAdapter(getActivity(), actionsArray);
+                    list.setAdapter(adapter);
+                });
+
+            }
+        });
     }
 }
